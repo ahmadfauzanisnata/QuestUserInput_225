6b5b3a925a2eb5b953d54f88fded1259b8b0c1bb
ahmad fauzan isnata <160833065+ahmadfauzanisnata@users.noreply.github.com>
2025-10-30 08:53:57 +0700

    Tambah state untuk input jeniskelamin

--- app/src/main/java/com/example/myapplication/TeksEdit.kt
+++ app/src/main/java/com/example/myapplication/TeksEdit.kt
@@ -6,3 +6,4 @@ fun FormDataDiri(modifier: Modifier) {
     // variabel-variabel untuk mengingat nilai masukan dari keyboard
     var textNama by remember { mutableStateOf(value = "") }
     var textAlamat by remember { mutableStateOf(value = "") }
+    var textJK by remember { mutableStateOf(value = "") }
