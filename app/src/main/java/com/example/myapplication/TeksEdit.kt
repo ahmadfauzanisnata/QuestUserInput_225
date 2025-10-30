1ee4701a48cf5d035399e92632fe5ae2722fada3
ahmad fauzan isnata <160833065+ahmadfauzanisnata@users.noreply.github.com>
2025-10-30 09:34:15 +0700

    Atur ketebalan dan warna divider

--- app/src/main/java/com/example/myapplication/TeksEdit.kt
+++ app/src/main/java/com/example/myapplication/TeksEdit.kt
@@ -83,6 +83,9 @@ fun FormDataDiri(modifier: Modifier) {
                 top = dimensionResource(
                     id = R.dimen.padding_medium
                 )),
-           
+            thickness = dimensionResource(id =R.dimen.divider_tipis),
+            color = Color.DarkGray
+        )
+
     }
 }
