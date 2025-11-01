3906191d9f2a91055838c28154178053a2320d58
ahmad fauzan isnata <160833065+ahmadfauzanisnata@users.noreply.github.com>
2025-11-01 12:07:34 +0700

    Inisialisasi state dan data untuk form

--- app/src/main/java/com/example/myapplication/TeksEdit.kt
+++ app/src/main/java/com/example/myapplication/TeksEdit.kt
@@ -1,134 +1,16 @@
 package com.example.myapplication
 
-import android.R.attr.text
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Button
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.ElevatedCard
-import androidx.compose.material3.HorizontalDivider
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.MaterialTheme.shapes
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.RadioButton
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.dp
+
+
 
 
 @Composable
-fun FormDataDiri(modifier: Modifier) {
+fun FormDataDiri(modifier: Modifier = Modifier) {
 
-    // variabel-variabel untuk mengingat nilai masukan dari keyboard
-    var textNama by remember { mutableStateOf(value = "") }
-    var textAlamat by remember { mutableStateOf(value = "") }
-    var textJK by remember { mutableStateOf(value = "") }
+    var nama by remember { mutableStateOf("") }
+    var alamat by remember { mutableStateOf("") }
+    var jenisKelamin by remember { mutableStateOf("") }
+    var status by remember { mutableStateOf("") }
 
-    // variabel-variabel untuk menyimpan data yang diperoleh dari komponen input
-    var nama by remember { mutableStateOf(value = "") }
-    var alamat by remember { mutableStateOf(value = "") }
-    var jenis by remember { mutableStateOf(value = "") }
-
-    val gender: List<String> = listOf("Laki-laki", "Perempuan")
-
-    Column(
-        modifier = Modifier.padding(top = 50.dp),
-        verticalArrangement = Arrangement.Top,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        val large = null
-        OutlinedTextField(
-            value = textNama,
-            singleLine = true,
-            shape = MaterialTheme,shapes,large,
-            modifier = Modifier.width(width = 250.dp),
-            label = { Text(text = "Nama Lengkap")},
-            onValueChange = {
-                textNama = it
-            }
-
-        )
-        Row {
-            gender.forEach { item ->
-                val textJk = null
-                Row(modifier = Modifier.selecttable(
-                    selected = textJk == item,
-                    onClick = {textJK = item}
-                ), verticalAlignment = Alignment.CenterVertically) {
-                    RadioButton(
-                        selected = textJK == item,
-                        onClick = {
-                            Text(text = item)
-                        })
-                    Text(text = item)
-                }
-            }
-        }
-        OutlinedTextField(
-            value = textAlamat,
-            singleLine = true,
-            modifier = Modifier.width(width = 250.dp),
-            label = { Text(text = "Alamat Lengkap")},
-            onValueChange = {
-                textAlamat = it
-            }
-        )
-        HorizontalDivider(
-            modifier = Modifier.padding(
-                bottom = dimensionResource(R.dimen.padding_medium),
-                top = dimensionResource(
-                    id = R.dimen.padding_medium
-                )),
-            thickness = dimensionResource(id =R.dimen.divider_tipis),
-            color = Color.DarkGray
-        )
-        Button(
-            modifier = Modifier.fillMaxWidth(fraction =1f),
-
-            enabled = textAlamat.isNotEmpty(),
-            onClick = {
-                nama=textNama
-                jenis=textJK
-                alamat=textAlamat
-            }
-        ){
-            Text(text = stringResource(id = R.string.Submit))
-        }
-        HorizontalDivider(
-            modifier = Modifier.padding(
-                bottom = dimensionResource(id = R.dimen.padding_medium),
-                top = dimensionResource(
-                    id = R.dimen.padding_medium
-                )),
-            thickness = dimensionResource(R.dimen.divider_tipis),
-            color = Color.DarkGray
-        )
-        ElevatedCard (
-            elevation = CardDefaults.cardElevation(defaultElevation = 10.dp),
-            colors = CardDefaults.cardColors(containerColor = Color.Black),
-            modifier = Modifier
-                .height(100.dp)
-                .width(300.dp)
-        ) {
-            Column (modifier = Modifier.padding(horizontal = 5.dp, vertical = 15.dp),){
-                Text(text = "Nama   : $nama", color = Color.White )
-                Text(text = "Gender : $jenis", color = Color.White )
-                Text(text = "Alamat : $alamat", color = Color.White)
-            }
-        }
-    }
-}
+    val listGender = listOf("Laki-laki", "Perempuan")
+    val listStatus = listOf("Janda", "Lajang", "Duda")
